16cd4ee51071bcc864b555dbe1f481b48d8e330b
Joao Carlos Morais <jcmroais.araujo@gmail.com>
2021-09-19 08:36:48 -0300

    Operadores relacionais

---
+++ exemplo5.pl
@@ -0,0 +1,16 @@
+$var1 = 4;
+$var2 = 2;
+
+# if ($var1 > $var2) {
+#     print "A variável 1 é maior que a variável 2";
+# }
+# else {
+#     print "A variável 2 é maior.";
+# };
+
+if (($var1 % $var2 == 0)) {
+    print "O número é par";
+}
+else {
+    print "O número é ímpar";
+}
