ec931d4f2f60db3605a170ecb83e1021507b76f2
Joao Carlos Morais <jcmroais.araujo@gmail.com>
2021-09-19 08:28:35 -0300

    operacoes matematica

---
+++ exemplo4.pl
@@ -0,0 +1,13 @@
+#Operadores Aritméticos
+
+# - -> subtração
+# + -> adição
+# * -> multiplicacao
+# / -> divisao
+# ** -> exponenciacao
+# % -> modulo 
+
+$var1 = 5;
+$var2 = 2;
+
+print $var1 % $var2;
