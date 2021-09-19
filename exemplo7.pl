f3cb0d1b6c804f867de6267e6d5f66e083b5fc94
Joao Carlos Morais <jcmroais.araujo@gmail.com>
2021-09-19 10:08:35 -0300

    Operadores de strings

---
+++ exemplo7.pl
@@ -0,0 +1,20 @@
+# Operadores de strings: promover a modificação em cadeias de caracteres
+
+# Operadores de incremento: representado por ++, vão agir incrementado o ultimo caractere de uma cadeira de caracteres
+
+$var1 = "b";
+$var1++;
+
+# print $var1;
+
+# Operador de concatenação: representando por um unico caractere de ponto ( . ) -> promove a união de duas cadeias de caracteres
+
+$var1 = "João";
+$var2 = "Carlos";
+
+# print $var1 . " " . $var2;
+
+# Operador de repetição: representao por um caractere x (minúsculo);
+
+$var1 = "Estou feliz!" x 3;
+print $var1 . "\n";
