c46b71e66c8882499f414578ac9b98ec5504ec6d
Joao Carlos Morais <jcmroais.araujo@gmail.com>
2021-09-20 05:50:22 -0300

    estrutura de repeticao

---
+++ exemplo9.pl
@@ -0,0 +1,15 @@
+$var1 = 5;
+
+if ($var1 == 1) {
+    print ("O valor de Var1 é 1.")
+}elsif ($var1 == 2) {
+   print ("O valor de Var1 é 2.")
+}elsif ($var1 == 3) {
+   print ("O valor de Var1 é 3.")
+}elsif ($var1 == 4) {
+   print ("O valor de Var1 é 4.")
+}elsif ($var1 == 5) {
+   print ("O valor de Var1 é 5.")
+}else {
+    print("O valor de var1 é diferente")
+}
